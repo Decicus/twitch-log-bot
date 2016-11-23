4929cd7c0988ca7931101550f93844e02e67014b
Alex Thomassen <alex@thomassen.xyz>
2016-11-23 23:15:07 +0100

    Add support for offset
    Also adds support for displaying messages in a "plaintext" format.

--- helpers.js
+++ helpers.js
@@ -22,4 +22,47 @@ helpers.now = () => {
     return date.join(" ") + " UTC";
 };
 
+/**
+ * Formats the timestamp into a UTC date.
+ *
+ * @param  {Mixed} timestamp The timestamp to format.
+ * @return {String}          The formatted date.
+ */
+helpers.formatDate = (timestamp) => {
+    timestamp = parseInt(timestamp);
+
+    let date = new Date(timestamp).toUTCString().split(" ");
+    date.shift();
+    date.pop();
+
+    return date.join(" ");
+};
+
+/**
+ * Retrieves either the header or query parameter value from the request.
+ *
+ * @param  {Object} req  The request object.
+ * @param  {String} name The name of the header/query parameter.
+ * @return {Mixed}       The value or undefined.
+ */
+helpers.requestValue = (req, name) => {
+    return (typeof req.query[name] !== 'undefined' ? req.query[name] : req.get(name));
+};
+
+/**
+ * Returns the proper response based on the input value.
+ *
+ * @param  {Object} res   The express response object.
+ * @param  {Mixed} value  The value to send back in the response.
+ * @return {Mixed}
+ */
+helpers.response = (res, value) => {
+    if (typeof value !== 'object') {
+        res.setHeader('Content-Type', 'text/plain');
+        return res.send(value);
+    }
+
+    return res.json(value);
+};
+
 module.exports = helpers;
